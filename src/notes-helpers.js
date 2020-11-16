4ac98a09b51003b16aa02d23928bddf8d2c70208
Danny Shi <dannyshi.sr@gmail.com>
2020-11-16 17:33:38 -0500

    ready to deploy to vercel

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,15 @@
 
-export const findFolder = (folders=[], folderId) =>
-  folders.find(folder => folder.id === folderId)
+export const findFolder = (folders = [], folderid) =>
+  folders.find(folder => folder.id === folderid)
 
-export const findNote = (notes=[], noteId) =>
+export const findNote = (notes = [], noteId) =>
   notes.find(note => note.id === noteId)
 
-export const getNotesForFolder = (notes=[], folderId) => (
+export const getNotesForFolder = (notes = [], folderId) => (
   (!folderId)
     ? notes
-    : notes.filter(note => note.folderId === folderId)
+    : notes.filter(note => note.folderid === folderId)
 )
 
-export const countNotesForFolder = (notes=[], folderId) =>
-  notes.filter(note => note.folderId === folderId).length
+export const countNotesForFolder = (notes = [], folderid) =>
+  notes.filter(note => note.folderid === folderid).length
